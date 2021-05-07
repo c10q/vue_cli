3396c135c319c90c2d6e1dc714800264a544c06f
knoh <knoh@seoulpass.kr>
2021-05-07 10:56:54 +0900

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model="msg">
+
+    <ul>
+      <li v-for="(item, index) in items" v-bind:key="index" v-on:click="sub">
+        {{ item.message }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,6 +20,21 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  computed: {},
+  watch: {},
+  data() {
+    return {
+      msg: 'okay',
+      items: [
+        {message: 'Foo'},
+        {message: 'Bar1'},
+        {message: 'Bar2'},
+        {message: 'Bar3'},
+      ]
+    }
   }
 }
 </script>
+
+<style></style>
