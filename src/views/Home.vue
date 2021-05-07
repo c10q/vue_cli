b64da0e145635010e0e77576b303ae1b6edad9c4
kwanok <cloq@kakao.com>
2021-05-07 17:20:55 +0900

    1. 회원가입, 로그인 버튼 추가 2. 라우터 설명 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="home">
-    <!--img alt="Vue logo" src="../assets/logo.png"-->
-    <button><router-link to="/about">누르세요</router-link></button>
-      <div><el-radio v-model="radio" label="1">강의듣는다</el-radio>
-      <el-radio v-model="radio" label="2">안듣는다</el-radio></div>
+    <button>
+      <router-link to="/about">누르세요</router-link>
+    </button>
+    <div>
+      <el-radio v-model="radio" label="1">강의듣는다</el-radio>
+      <el-radio v-model="radio" label="2">안듣는다</el-radio>
+    </div>
     <input type="text" v-model="msg">
     <ul>
       <li v-for="(item, index) in items" v-bind:key="index">
         {{ item.message }}
       </li>
     </ul>
+    <el-button @click="signUp">SIGNUP</el-button>
+    <el-button @click="signIn" type="primary">SIGNIN</el-button>
   </div>
 </template>
 
@@ -17,11 +22,6 @@
 
 export default {
   name: 'Home',
-  components: {
-    
-  },
-  computed: {},
-  watch: {},
   data() {
     return {
       radio: '1',
@@ -34,15 +34,30 @@ export default {
         {message: '청계천'},
       ]
     }
+  },
+  methods: {
+    signIn() {
+      // 이건 프로그래밍 기반 라우팅인데
+      // template 에서 router-link 로 페이지 이동을 하잖아요
+      // 그런데 javascript 명령어로도 라우팅을 할 수 있어요
+      // 이렇게 프로그래밍 기반 라우팅을 사용하면 단순히 이동하는 것 외의 더 많은 기능을 쓸 수 있어요
+      //
+      // 그냥 이게 좀 더 좋아요
+      this.$router.push('/signIn')
+    },
+    signUp() {
+      this.$router.push('/signUp')
+    }
   }
 }
 </script>
 
 <style>
-*{
+* {
   margin: 7px;
 }
-li{
+
+li {
   list-style: none;
 }
 </style>
