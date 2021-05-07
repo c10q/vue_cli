aeff5da7216e6c6898ce19ed10a46b0e21e90957
kwanok <cloq@kakao.com>
2021-05-07 17:22:04 +0900

    서버(Firebase) 기본 설정 설명

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'firebase/auth'
-import "firebase/firestore";
-import firebase from "firebase";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import firebase from "firebase"
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
 
-let firebaseConfig = {
+let firebaseConfig = { // 파이어베이스 설정 정보
   apiKey: "AIzaSyA_wdrEhnY9ioj4qD9buaPOOMY4W1gedXk",
   authDomain: "fwtalk.firebaseapp.com",
   projectId: "fwtalk",
@@ -17,11 +16,13 @@ let firebaseConfig = {
   messagingSenderId: "753398946190",
   appId: "1:753398946190:web:79d34bd13b789194e82cdf",
   measurementId: "G-E9D3MJEG4T"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-Vue.use(ElementUI);
+}
+
+firebase.initializeApp(firebaseConfig) // 파이어베이스 init
+firebase.analytics() // analytics 사용하겠다는 뜻
+
+Vue.use(ElementUI)
+
 new Vue({
   router,
   store,
