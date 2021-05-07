c1d7e46c4321dce453c26e59200ceddc1c72d911
kwanok <cloq@kakao.com>
2021-05-07 18:15:38 +0900

    숙제 추가

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,13 +1,19 @@
 <template>
   <el-card>
     id: <el-input type="text" v-model="email" />
-    password: <el-input type="text" v-model="password" />
+    password: <el-input type="password" v-model="password" />
     <el-button @click="signIn">SIGN IN</el-button>
+    <el-button @click="signOut">SIGN OUT</el-button>
   </el-card>
 </template>
 
 <script>
+import firebase from "firebase"
+
 export default {
+  created() { // vue 라이프 사이클 중 beforeCreated 이후 실행되는 과정
+    this.checkLoggedIn() // 밑에 로그인 체크 메서드 실행
+  },
   name: "SignIn",
   data() {
     return {
@@ -17,7 +23,35 @@ export default {
   },
   methods: {
     signIn() {
-      console.log('로그인')
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+          .then((userCredential) => {
+            // Todo 홈으로 리다이렉트
+            let user = userCredential.user
+
+            console.log(user)
+          })
+          .catch((error) => {
+            // Todo 로그인 실패 Alert
+            let errorCode = error.code
+            let errorMessage = error.message
+
+            console.log(errorCode)
+            console.log(errorMessage)
+          });
+    },
+    signOut() {
+      firebase.auth().signOut()
+    },
+    checkLoggedIn() {
+      // 이건 서버에서 계속 관찰하는 과정인데 인증 상태가 업데이트되면 실행돼요
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          let uid = user.uid
+          alert(uid + '유저가 접속중이다')
+        } else {
+          alert('로그인 된 사람이 없다')
+        }
+      });
     }
   }
 }
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -20,27 +20,18 @@ export default {
   name: 'SignUp',
   data() {
     return {
-      email: '',
-      password: '',
+      email: '', // Todo 회원가입 전 이메일 폼 체크
+      password: '', // Todo 패스워드 6글자 미만이면 경고 Alert
     }
   },
   methods: {
     signUp() {
-      let db = firebase.firestore() // 데이터베이스 쓰겠다고 선언
-
       firebase.auth().createUserWithEmailAndPassword(this.email, this.password) // 유저 생성해주세요
-          .then((userCredential) => { // 콜백 받으면
+          .then((userCredential) => { // then: 콜백 받으면
             let user = userCredential.user // user 변수에 콜백 받은 결과를 담아서
-            db.collection("users").add({ // 데이터베이스 (users 폴더) 에 추가
-              // 파이어스토어는 키-값으로 데이터를 저장해요
-              email: user.email, // 키: email 값: user.email
-            }).then((docRef) => {
-              // 문서 아이디는 db.collection().add() 로 데이터 추가하고 무작위로 생성되는데
-              // 저희가 쓰는 uid 비슷해요
-              console.log("문서 ID: ", docRef.id);
-            }).catch((error) => { // 캐치는 오류 처리, 오류가 생기면 아래 코드가 실행돼요
-              console.error("문서 생성 오류 ", error);
-            });
+            this.saveInfo(user) // 밑에 메서드 비동기 호출 -> 호출하고 바로 다음 코드 실행
+            // Todo 메서드 앞에 this 붙이는 이유 공부
+            // Todo 로그인 페이지로 리다이렉트
           })
           .catch((error) => {
             let errorCode = error.code
@@ -49,8 +40,24 @@ export default {
             console.log(errorCode)
             console.log(errorMessage)
           });
+    },
+    saveInfo(user) {
+      let db = firebase.firestore() // 데이터베이스 쓰겠다고 선언
+      let now = new Date // 현재의 Date 객체를 생성
+
+      db.collection("users").add({ // 데이터베이스 (users 폴더) 에 추가
+        // 파이어스토어는 키-값으로 데이터를 저장해요
+        email: user.email, // 키: email 값: user.email
+        created_at: now
+      }).then((docRef) => { // 콜백 받으면
+        // 문서 아이디는 db.collection().add() 로 데이터 추가하고 무작위로 생성되는데
+        // 저희가 쓰는 uid 비슷해요
+        console.log("문서 ID: ", docRef.id);
+      }).catch((error) => { // 캐치는 오류 처리, 오류가 생기면 아래 코드가 실행돼요
+        console.error("문서 생성 오류 ", error);
+      });
     }
-  }
+  },
 }
 </script>
 
