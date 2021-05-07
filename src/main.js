bf11dd939fbc2a18e207b9901505985c535376c2
knoh <knoh@seoulpass.kr>
2021-05-07 12:37:10 +0900

    commit 테스트

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+import 'firebase/auth'
+import "firebase/firestore";
 
 Vue.config.productionTip = false
+
+let firebaseConfig = {
+  apiKey: "AIzaSyA_wdrEhnY9ioj4qD9buaPOOMY4W1gedXk",
+  authDomain: "fwtalk.firebaseapp.com",
+  projectId: "fwtalk",
+  storageBucket: "fwtalk.appspot.com",
+  messagingSenderId: "753398946190",
+  appId: "1:753398946190:web:79d34bd13b789194e82cdf",
+  measurementId: "G-E9D3MJEG4T"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
