6de792a6e14785100d1bf3fb8d84639a5f2d3fdc
c10q <cloq@kakao.com>
2021-05-09 00:37:43 +0900

    vuex init

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from "firebase";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    test: '테스트 상태',
+    user: null
   },
   mutations: {
+    setUser(state) {
+      state.user = firebase.auth().currentUser.email
+    },
+    removeUser(state) {
+      state.user = null
+    }
   },
   actions: {
   },
