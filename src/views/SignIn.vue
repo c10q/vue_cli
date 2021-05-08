a4539f8b7b1aecdcc49639efa06657077c60a9ee
c10q <cloq@kakao.com>
2021-05-09 00:37:31 +0900

    로그인 실패처리

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -3,19 +3,19 @@
     <h2 class="sign-in-title">LOGIN</h2>
 
     <div>
-      <span>EMAIL</span>
-      <el-input type="text" v-model="email" class="sign-up-input"/>
+      <span>Email</span>
+      <el-input type="text" v-model="email" class="sign-in-input"/>
     </div>
 
     <div>
-      <span>PASSWORD</span>
-      <el-input type="password" v-model="password" class="sign-up-input"/>
+      <span>Password</span>
+      <el-input type="password" v-model="password" class="sign-in-input" v-on:keyup.native.enter="signIn"/>
     </div>
 
-    <el-divider />
+    <el-divider/>
 
     <div class="sign-in-button-group">
-      <el-button @click="signOut">회원가입</el-button>
+      <el-button @click="signUp">회원가입</el-button>
       <el-button @click="signIn" type="primary">로그인</el-button>
     </div>
 
@@ -26,9 +26,6 @@
 import firebase from "firebase"
 
 export default {
-  created() { // vue 라이프 사이클 중 beforeCreated 이후 실행되는 과정
-    this.checkLoggedIn() // 밑에 로그인 체크 메서드 실행
-  },
   name: "SignIn",
   data() {
     return {
@@ -40,33 +37,40 @@ export default {
     signIn() {
       firebase.auth().signInWithEmailAndPassword(this.email, this.password)
           .then((userCredential) => {
-            // Todo 홈으로 리다이렉트
             let user = userCredential.user
+            this.$router.push({name: 'Home'})
 
             console.log(user)
           })
           .catch((error) => {
-            // Todo 로그인 실패 Alert
             let errorCode = error.code
             let errorMessage = error.message
 
-            console.log(errorCode)
-            console.log(errorMessage)
+            switch(errorCode) {
+              case 'auth/invalid-email':
+                this.$message({
+                  message: '이메일 형식을 확인해주세요',
+                  type: "warning"
+                })
+                break
+              case 'auth/wrong-password':
+                this.$message({
+                  message: '이메일 형식을 확인해주세요',
+                  type: "warning"
+                })
+                break
+              default:
+                this.$message({
+                  message: errorMessage,
+                  type: "error"
+                })
+                console.log(errorCode)
+                console.log(errorMessage)
+            }
           });
     },
-    signOut() {
-      firebase.auth().signOut()
-    },
-    checkLoggedIn() {
-      // 이건 서버에서 계속 관찰하는 과정인데 인증 상태가 업데이트되면 실행돼요
-      firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          let uid = user.uid
-          console.log(uid + '유저가 접속중이다')
-        } else {
-          console.log('로그인 된 사람이 없다')
-        }
-      });
+    signUp() {
+      this.$router.push({name: 'SignUp'}).catch(() => {})
     }
   }
 }
@@ -77,7 +81,7 @@ export default {
   margin-bottom: 32px;
 }
 
-.sign-up-input {
+.sign-in-input {
   width: 95%;
   margin: 0;
 }
